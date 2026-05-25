02d8f7aed4d02e5aae7e10db7e1cd2dc32e13220
ankita9142 <siyuapiyu@gmail.com>
2026-05-25 12:24:46 +0530

    Update login.py

--- face_reco/pages/login.py
+++ face_reco/pages/login.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pymongo
-myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")
+myclient = pymongo.MongoClient("mongodb+srv://Ankita_9241:<db_password>@cluster0.hngpsnl.mongodb.net/?appName=Cluster0")
 mydb = myclient["face"]
 my=mydb["user_info"]
 
